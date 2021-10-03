347103142c3d6768b1e67722a8978234c584be72
DAF201 <56328675+DAF201@users.noreply.github.com>
2021-10-03 18:02:06 -0500

    Update self.py

--- self.py
+++ self.py
@@ -1,16 +1,18 @@
 
-times=0
-times=times+1
-this=__file__
-if times==1:
-    with open(__file__,'r')as this_file:
-        this=this_file.readlines()[2:]
+# don't delete the line above, important!
+times = 0
+times = times+1
+this = __file__
+print("I has run for %s times!" % times)
+if times == 1:
+    with open(__file__, 'r')as this_file:
+        this = this_file.readlines()[2:]
 else:
-    with open(__file__,'r')as this_file:
-        this=this_file.readlines()[1:]
-temp=''
+    with open(__file__, 'r')as this_file:
+        this = this_file.readlines()[1:]
+temp = ''
 for x in this:
-    temp=temp+x
-this=temp
-with open(__file__,'w')as file:
-    file.write('times=%s\n%s'%(times,this))
+    temp = temp+x
+this = temp
+with open(__file__, 'w')as file:
+    file.write('times=%s\n%s' % (times, this))
