0506324f123916e589826432dd2f762e314d2c75
DAF201 <56328675+DAF201@users.noreply.github.com>
2021-10-03 18:38:19 -0500

    Update counter.py

--- counter.py
+++ counter.py
@@ -1,15 +1,9 @@
-
-# don't delete the line above, important!
-times = 0
+times=0
 times = times+1
 this = __file__
 print("I has run for %s times!" % times)
-if times == 1:
-    with open(__file__, 'r')as this_file:
-        this = this_file.readlines()[2:]
-else:
-    with open(__file__, 'r')as this_file:
-        this = this_file.readlines()[1:]
+with open(__file__, 'r')as this_file:
+    this = this_file.readlines()[1:]
 temp = ''
 for x in this:
     temp = temp+x
