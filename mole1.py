cf8fed2ac66d03781a060923b268520b62c62d95
DAF201 <56328675+DAF201@users.noreply.github.com>
2021-10-03 21:53:21 -0500

    Update mole1.py

--- mole1.py
+++ mole1.py
@@ -1,4 +1,4 @@
-times = 1
+times = 0
 PATH=''
 #this is the first mole!
 times = times+1
@@ -6,7 +6,6 @@ this = __file__
 
 import subprocess
 import os
-import sys
 def change():
     try:
         with open(__file__, 'r')as this_file:
@@ -18,13 +17,13 @@ def change():
         with open("mole"+str(times)+".py", 'w')as file:
             file.write('times=%s\nPATH=r"%s"\n%s' % (times, __file__, this))
     except:
-        pass
+        change()
     finally:
         print('this is the %s mole!'%(times-1))
-    subprocess.Popen("python mole"+str(times)+".py")
-    if times==1:
-        sys.exit(0)
-    else:
+    try:
+        subprocess.Popen("python mole"+str(times)+".py")
         os.remove(PATH)
-        
+    except:
+        subprocess.Popen("python mole"+str(times)+".py")
+        os.remove(PATH)
 change()
