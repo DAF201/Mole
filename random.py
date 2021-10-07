4b95fbe80b6b86dd62e3e220408007b3fee19ceb
DAF201 <56328675+DAF201@users.noreply.github.com>
2021-10-07 00:09:51 -0500

    Delete random.py

--- random.py
+++
@@ -1,9 +0,0 @@
-import os
-import random
-directories = [row[0] for row in os.walk(os.path.expanduser('~/subdir'))]
-directories = []
-for i, row in enumerate(os.walk(os.path.expanduser('~'))):
-    if i > 64:
-        break
-    directories += [row[0]]
-print(random.choice(directories))
